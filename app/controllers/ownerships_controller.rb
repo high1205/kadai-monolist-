2f720bb3e34d0c649227d50d38c3a416b97a068f
toru.takahashi <asset.management.1983@gmail.com>
2018-09-03 12:47:38 +0000

    have function

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -13,7 +13,13 @@ class OwnershipsController < ApplicationController
     # Want 関係として保存
     if params[:type] == 'Want'
       current_user.want(@item)
-      flash[:success] = '商品を Want しました。'
+      flash[:success] = '商品を希望リストに入れました。'
+    end
+    
+    # Have 関係として保存
+    if params[:type] == 'Have'
+      current_user.have(@item)
+      flash[:success] = '商品を既保有リスト入れました。'
     end
 
     redirect_back(fallback_location: root_path)
@@ -21,10 +27,17 @@ class OwnershipsController < ApplicationController
 
   def destroy
     @item = Item.find(params[:item_id])
-
+    
+    # Want 関係から解除
     if params[:type] == 'Want'
       current_user.unwant(@item) 
-      flash[:success] = '商品の Want を解除しました。'
+      flash[:danger] = '商品を希望リストから削除しました。'
+    end
+    
+    # Have 関係から解除
+    if params[:type] == 'Have'
+      current_user.unhave(@item) 
+      flash[:danger] = '商品を既保有リストから削除しました。'
     end
 
     redirect_back(fallback_location: root_path)
